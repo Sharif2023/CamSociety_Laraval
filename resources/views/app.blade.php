0e52ce8f3ff9887d5c549cd18ada259bc724f7ed
Sharif2023 <sharifislam0505@gmail.com>
2026-01-08 18:26:47 +0600

    add logo to all page title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link rel="icon" type="image/png" href="/images/logo.png" />
+        <link rel="icon" type="image/jpeg" href="/images/camSociety_logo.jpg" />
 
         <!-- Scripts -->
         @routes
